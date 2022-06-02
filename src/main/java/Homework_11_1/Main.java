68f53d00bd26647776b4c6b193331283f2a84cea
IhorBorysov <95441954+IhorBorysov@users.noreply.github.com>
2022-06-02 09:26:29 +0300

    Homework_11.1

---
+++ src/main/java/Homework_11_1/Main.java
@@ -0,0 +1,26 @@
+package Homework_11_1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    static void patt_a(String text) {
+        Pattern pattern = Pattern.compile("a.+?\s", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+        ArrayList<String> texts = new ArrayList<String>();
+
+        while (matcher.find()) {
+            texts.add(matcher.group());
+            System.out.println("В масив було додано слова -> " + texts);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.print("Введіть слова: ");
+        String text;
+        Scanner scanner = new Scanner(System.in);
+        text = scanner.nextLine();
+        patt_a(text);
+    }
+}
